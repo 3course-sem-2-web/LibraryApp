76047604638e9f52e68669509c0978fc3e8b1b0e
KhomenkoMac <maksim_khomenko@ukr.net>
2023-05-30 22:38:53 +0300

    feat: books app + identity

--- angularapp/src/proxy.conf.js
+++ angularapp/src/proxy.conf.js
@@ -1,11 +1,11 @@
-const PROXY_CONFIG = [
-  {
-    context: [
-      "/weatherforecast",
-    ],
-    target: "https://localhost:7036",
-    secure: false
-  }
-]
+// const PROXY_CONFIG = [
+//   {
+//     context: [
+//       "/",
+//     ],
+//     target: "https://localhost:7036",
+//     secure: false
+//   }
+// ]
 
-module.exports = PROXY_CONFIG;
+// module.exports = PROXY_CONFIG;
